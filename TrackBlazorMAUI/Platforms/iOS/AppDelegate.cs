85d5dd9ebc2a21dcafae348e28e1ad5e2a1dd9f0
M.Fedorchenko <M.Fedorchenko@00-001204>
2023-03-12 01:33:04 +0600

    Add project files.

---
+++ TrackBlazorMAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TrackBlazorMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
